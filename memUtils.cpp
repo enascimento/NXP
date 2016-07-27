86a35ec420973b02a0f086e7bd026cacf0d835f3
Siemen <siemen.d@hotmail.com>
2016-07-27 11:42:07 +0200

    memUtils is added

---
+++ memUtils.cpp
@@ -0,0 +1,152 @@
+/* ===================================================================== */
+/* This file is part of Daredevil                                        */
+/* Daredevil is a side-channel analysis tool                             */
+/* Copyright (C) 2016                                                    */
+/* Original author:   Paul Bottinelli <paulbottinelli@hotmail.com>       */
+/* Contributors:      Joppe Bos <joppe_bos@hotmail.com>                  */
+/*                    Philippe Teuwen <phil@teuwen.org>                  */
+/*                                                                       */
+/* This program is free software: you can redistribute it and/or modify  */
+/* it under the terms of the GNU General Public License as published by  */
+/* the Free Software Foundation, either version 3 of the License, or     */
+/* any later version.                                                    */
+/*                                                                       */
+/* This program is distributed in the hope that it will be useful,       */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of        */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         */
+/* GNU General Public License for more details.                          */
+/*                                                                       */
+/* You should have received a copy of the GNU General Public License     */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+/* ===================================================================== */
+
+
+/* This file deals with general function that help with memory management
+ * which are used throughout the rest of the code.
+ */
+
+
+#include <typeinfo>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "memUtils.h"
+
+/* Returns the number of columns that can be loaded in memory.
+ */
+  template <typename Type>
+int get_ncol(long int memsize, int ntraces)
+{
+  // We use 60% of the available memory. We never know what can happen :)
+  return (0.6*memsize)/(sizeof(Type)*ntraces);
+}
+
+
+  template <class Type>
+void free_matrix(Type *** matrix, int n_rows)
+{
+  for (int i=0; i < n_rows; i++) {
+    free((*matrix)[i]);
+  }
+  free(*matrix);
+}
+
+/* Allocates the array matrix
+ */
+  template <class Type>
+int allocate_matrix(Type *** matrix, int n_rows, int n_columns)
+{
+  *matrix = (Type **)malloc(n_rows * sizeof(Type *));
+  if(*matrix == NULL)
+    return -1;
+
+  for (int i=0; i < n_rows; i++) {
+    (*matrix)[i] = (Type *) malloc (n_columns * sizeof(Type));
+    if((*matrix)[i] == NULL)
+      return -1;
+  }
+  return 0;
+}
+
+/* This functions simply splits the total_work (usually represents the number
+ * of columns of the matrix we're processing) into an equal number of threads,
+ * creates this amount of threads and starts them with the function fct.
+ */
+  template <class TypeTrace, class TypeReturn, class TypeGuess>
+int split_work(FinalConfig<TypeTrace, TypeReturn, TypeGuess> & fin_conf, void * (*fct)(void *), TypeReturn ** precomp_k, int total_work, int offset)
+{
+  int n, rc,
+      workload = 0,
+      n_threads = fin_conf.conf->n_threads,
+      /* Can be changed later in order to compute on less traces.
+       */
+      n_traces = fin_conf.conf->total_n_traces;
+
+
+  /* If the total work by thread is smaller than 1, only the last thread would
+   * work, which is against the sole principle of multithreading. Thus, we
+   * reduce the number of threads until the workload is larger than 1.
+   *
+   * This is quite a naive approach, and it would be better to look into more
+   * efficient load balancing algorithms.
+   *
+   */
+  workload = (total_work/n_threads);
+  while (workload < 1) {
+    n_threads -= 1;
+    workload = (total_work/n_threads);
+  }
+
+  pthread_t threads[n_threads];
+  General<TypeTrace, TypeReturn, TypeGuess> *ta = NULL;
+
+  ta = (General<TypeTrace, TypeReturn, TypeGuess> *) malloc(n_threads * sizeof(General<TypeTrace, TypeReturn, TypeGuess>));
+  if (ta == NULL) {
+    fprintf (stderr, "[ERROR] Memory alloc failed.\n");
+    return -1;
+  }
+
+  for (n = 0; n < n_threads; n++) {
+    //printf(" Thread_%i [%i-%i]\n", n, n*workload + offset, offset + n*workload + workload + ((n + 1) / n_threads)*(total_work % n_threads));
+    ta[n] = General<TypeTrace, TypeReturn, TypeGuess>(n*workload, workload + ((n + 1) / n_threads) * (total_work % n_threads), n_traces, offset, total_work, precomp_k, &fin_conf);
+    rc = pthread_create(&threads[n], NULL, (*fct), (void *) &ta[n]);
+    if (rc != 0) {
+      fprintf(stderr, "[ERROR] Creating thread.\n");
+      free (ta);
+      return -1;
+    }
+  }
+
+  for (n = 0; n < n_threads; n++) {
+    rc = pthread_join(threads[n], NULL);
+    if (rc != 0) {
+      fprintf(stderr, "[ERROR] Joining thread.\n");
+      free (ta);
+      return -1;
+    }
+  }
+  free (ta);
+  return 0;
+}
+
+template int get_ncol<int8_t>(long int memsize, int ntraces);
+template int get_ncol<float>(long int memsize, int ntraces);
+template int get_ncol<double>(long int memsize, int ntraces);
+
+template void free_matrix(float *** matrix, int n_rows);
+template void free_matrix(double *** matrix, int n_rows);
+template void free_matrix(uint8_t *** matrix, int n_rows);
+template void free_matrix(int8_t *** matrix, int n_rows);
+template void free_matrix(int *** matrix, int n_rows);
+
+template int allocate_matrix(float *** matrix, int n_rows, int n_columns);
+template int allocate_matrix(double *** matrix, int n_rows, int n_columns);
+template int allocate_matrix(uint8_t *** matrix, int n_rows, int n_columns);
+template int allocate_matrix(int8_t *** matrix, int n_rows, int n_columns);
+template int allocate_matrix(int *** matrix, int n_rows, int n_columns);
+
+template int split_work<double, double, uint8_t>(FinalConfig<double, double, uint8_t> & fin_conf, void * (*fct)(void *), double ** precomp_k, int total_work, int offset);
+template int split_work<float, double, uint8_t>(FinalConfig<float, double, uint8_t> & fin_conf, void * (*fct)(void *), double ** precomp_k, int total_work, int offset);
+template int split_work<int8_t, double, uint8_t>(FinalConfig<int8_t, double, uint8_t> & fin_conf, void * (*fct)(void *), double ** precomp_k, int total_work, int offset);
+template int split_work<float, float, uint8_t>(FinalConfig<float, float, uint8_t> & fin_conf, void * (*fct)(void *), float ** precomp_k, int total_work, int offset);
+template int split_work<int8_t, float, uint8_t>(FinalConfig<int8_t, float, uint8_t> & fin_conf, void * (*fct)(void *), float ** precomp_k, int total_work, int offset);
---
+++ memUtils.h
@@ -0,0 +1,241 @@
+/* ===================================================================== */
+/* This file is part of Daredevil                                        */
+/* Daredevil is a side-channel analysis tool                             */
+/* Copyright (C) 2016                                                    */
+/* Original author:   Paul Bottinelli <paulbottinelli@hotmail.com>       */
+/* Contributors:      Joppe Bos <joppe_bos@hotmail.com>                  */
+/*                                                                       */
+/* This program is free software: you can redistribute it and/or modify  */
+/* it under the terms of the GNU General Public License as published by  */
+/* the Free Software Foundation, either version 3 of the License, or     */
+/* any later version.                                                    */
+/*                                                                       */
+/* This program is distributed in the hope that it will be useful,       */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of        */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         */
+/* GNU General Public License for more details.                          */
+/*                                                                       */
+/* You should have received a copy of the GNU General Public License     */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+/* ===================================================================== */
+
+#ifndef MEMUTILS_H
+#define MEMUTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include "utils.h"
+
+#ifndef RESOURCES
+#define RESOURCES "/usr/share/daredevil"
+#endif //RESOURCES
+
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define RESET "\033[0m"
+
+#define GIGA 1e9
+#define MEGA 1e6
+
+using namespace std;
+
+/* Structure used to stores all the arrays.
+ */
+template <typename TypeTrace, typename TypeReturn, typename TypeGuess>
+struct MatArgs {
+
+  TypeTrace ** trace;
+  TypeGuess ** guess;
+  TypeReturn ** results;
+
+  MatArgs(TypeTrace ** tr, TypeGuess ** gues, TypeReturn ** res):
+    trace(tr), guess(gues), results(res) {
+    }
+};
+
+/* Structures used to stores all the *common* information needed by the
+ * threads to compute the correlation, like the pointers to the traces
+ * and guesses arrays and their sizes.
+ */
+template <typename TypeTrace, typename TypeReturn, typename TypeGuess>
+struct Args {
+
+  TypeTrace ** trace;
+  int n_samples;
+  TypeGuess ** guess;
+  int n_keys;
+  TypeReturn ** results;
+  int n_traces;
+  int nsqr;
+
+  Args(TypeTrace ** tr, int n_s, TypeGuess ** gues, int nk, TypeReturn ** res, int nt, int ns):
+    trace(tr), n_samples(n_s), guess(gues), n_keys(nk), results(res), n_traces(nt), nsqr(ns) {
+    }
+};
+
+/* Structure used by threads to access the common information, and the
+ * indices in the array of traces where every individual thread will start
+ * and stop computing correlations.
+ */
+template <typename TypeTrace, typename TypeReturn, typename TypeGuess>
+struct ThreadArgs {
+
+    Args<TypeTrace, TypeReturn, TypeGuess> * args;
+    int start;
+    int length;
+
+    ThreadArgs(Args<TypeTrace, TypeReturn, TypeGuess> * a, int st, int len):
+      args(a), start(st), length(len) {
+    }
+};
+
+/* Homemade Priority Queue used to store the best correlations
+ */
+template <typename Type>
+class PriorityQueue
+{
+  Type * array;
+  int size, max_size, index_min, total;
+
+  public:
+  PriorityQueue(int s)
+  {
+    init(s);
+  }
+  PriorityQueue(){}
+
+  void init(int s)
+  {
+    max_size = s;
+    array = (Type *) malloc(max_size * sizeof(Type));
+    size = 0;
+    index_min = 0;
+    total = 0;
+  }
+  void insert(const Type& elem)
+  {
+    if (size == max_size) {
+      if(array[index_min] < elem) {
+        array[index_min] = elem;
+        update_smallest_ind();
+      }
+    }else {
+      array[size] = elem;
+      if (elem < array[index_min]) {
+        index_min = size;
+      }
+      size += 1;
+    }
+    total++;
+  }
+
+  void print(int length = -1, int key = -1)
+  {
+    int i;
+    uint8_t seen_key = 0;
+
+    if (length == -1 || length > size)
+      length = size;
+
+    cout << "[INFO]\t" << total <<" correlations computed in total." << endl;
+    cout << "[INFO]\tGlobal top " << length << " correlations." << endl;
+    sort(array, array + size);
+    update_smallest_ind();
+    cout << endl;
+    for (i = size - 1; i >= (size-length); i--) {
+      if (array[i] == key){
+        seen_key = 1;
+        cout << KGRN << array[i] << RESET << endl;
+      }else
+        cout << array[i] << endl;
+    }
+
+    if(length != size && !seen_key){
+      while (i > 0){
+        if (array[i] == key){
+          seen_key = 1;
+          for(int j = 0; j < 3; j++)
+            cout << setw(13) << '.' << setw(10) << '.' <<  setw(10) << '.' <<setw(8) << '.' << endl;
+          cout << KGRN << array[i] << RESET << "\tat rank " << size-i << "." << endl;
+          break;
+        }
+        i--;
+      }
+    }
+    if (!seen_key && key != -1 && size != 0){
+      cout << endl;
+      cout << "Key 0x" << hex << key << " does not appear in the top " << dec << size << " correlations." << endl;
+    }
+    cout << endl;
+  }
+  private:
+  void update_smallest_ind()
+  {
+    int i;
+    for (i = 0; i < size; i++) {
+      if (array[i] < array[index_min]) {
+        index_min = i;
+      }
+    }
+  }
+
+};
+
+/* Structure used to store ALL the general and common information
+ */
+template <typename TypeTrace, typename TypeReturn, typename TypeGuess>
+struct FinalConfig {
+
+  MatArgs<TypeTrace, TypeReturn, TypeGuess> * mat_args;
+  Config * conf;
+  void * queues;
+
+  FinalConfig(MatArgs<TypeTrace, TypeReturn, TypeGuess> * m_a, Config * c, void * q):
+    mat_args(m_a), conf(c), queues(q){
+    }
+};
+
+//The files from socpa.h start here
+template <typename TypeTrace, typename TypeReturn, typename TypeGuess>
+struct General {
+
+  int start;
+  int length;
+  int n_traces;
+  int global_offset;
+  /* The number of columns that we can store in memory. Needed to know when to
+   * stop computing correlations in the last slice when computing on big files.
+   */
+  int n_samples;
+  TypeReturn ** precomp_guesses;
+  FinalConfig<TypeTrace, TypeReturn, TypeGuess> * fin_conf;
+
+  General(int st, int len, int nt, int go, int nc, TypeReturn ** pg, FinalConfig<TypeTrace, TypeReturn, TypeGuess> * s):
+    start(st), length(len), n_traces(nt), global_offset(go), n_samples(nc), precomp_guesses(pg), fin_conf(s){
+  }
+};
+
+/* Frees a matrix
+ */
+template <class Type>
+void free_matrix(Type *** matrix, int n_rows);
+
+/* Allocates memory for a matrix
+ */
+template <class Type>
+int allocate_matrix(Type *** matrix, int n_rows, int n_columns);
+
+template <typename Type>
+int get_ncol(long int memsize, int ntraces);
+
+template <class TypeTrace, class TypeReturn, class TypeGuess>
+int split_work(FinalConfig<TypeTrace, TypeReturn, TypeGuess> & fin_conf, void * (*fct)(void *), TypeReturn ** precomp_k, int total_work, int offset=0);
+
+#endif
